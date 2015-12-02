bc74972c5704d24542aa37986a1c00a35ac1be59
Ronan Lopes <lopesronanufsj@gmail.com>
2015-12-02 18:01:03 -0200

    Integrando com heroku

--- app/models/conta.rb
+++ app/models/conta.rb
@@ -1,3 +1,36 @@
 class Conta < ActiveRecord::Base
   belongs_to :user
+
+  #Verificar i18n
+  validates :saldo, :numericality => { :greater_than => 0 }
+
+  def saldo_to_s
+    "R$ #{self.saldo/100}"
+  end
+
+  def deposito(valor)
+    self.saldo += valor
+    self.save
+  end
+
+  def saque(valor)
+    self.saldo -= valor
+    self.save
+    logger.debug 88888888888888
+    logger.debug self.errors.full_messages
+  end
+
+  def self.transferencia(conta_origem, conta_destino, valor)
+    #Essa deve ser uma transação atômica
+    conta_origem -= valor
+    conta_destino += valor
+    conta_origem.save
+    conta_destino.save
+  end
+
+  def extrato(data_inicio, data_fim)
+    movimentacao.where()
+  end
+
+  
 end
